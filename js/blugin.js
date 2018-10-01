cdc83402fa5f10b3edb6928e227175a3beb5307e
Nemo <37085449+Amal-hossam@users.noreply.github.com>
2018-10-01 04:53:17 +0200

    js typed

--- js/blugin.js
+++ js/blugin.js
@@ -29,8 +29,8 @@ $(document).ready(function () {
             $(this).typed({
                 strings: [$(this).data('text1'), $(this).data('text2')],
                 loop: $(this).data('loop') ? $(this).data('loop') : false,
-                backDelay: $(this).data('backdelay') ? $(this).data('backdelay') : 2000,
-                typeSpeed: 10,
+                backDelay: $(this).data('backdelay') ? $(this).data('backdelay') : 500,
+                typeSpeed: 150,
             });
         });
     }
