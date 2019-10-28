3b50416ee43cd31a00bfb1cd8a8851ef4b62c306
Amal-hossam <amlhossamy@gmail.com>
2019-10-28 06:26:00 -0700

    updates and fixed errors

--- js/blugin.js
+++ js/blugin.js
@@ -41,30 +41,43 @@ $(document).ready(function () {
     }
 
 
-    //counter
-
-    var counters = $(".count");
-    var countersQuantity = counters.length;
-    var counter = [];
-
-    for (i = 0; i < countersQuantity; i++) {
-        counter[i] = parseInt(counters[i].innerHTML);
-    }
-
-    var count = function (start, value, id) {
-        var localStart = start;
-        setInterval(function () {
-            if (localStart < value) {
-                localStart++;
-                counters[id].innerHTML = localStart;
+    
+   
+// start counter
+    function countUp() {
+        $('.count').each(function() {
+            $(this).prop('Counter', 0).animate({
+              Counter: $(this).text()
+            }, {
+              duration: 4000,
+              easing: 'swing',
+              step: function(now) {
+                $(this).text(Math.ceil(now));
+              }
+            });
+          });
+        }
+        $(function() {
+          "user strict";
+          var bAnimate = true;
+          $(".count").css ("opacity", "0.0");
+          
+          $(window).scroll(function() {
+            // console.log("scroll top=" + $(this).scrollTop());
+            // console.log("div offset top=" + $("div").offset().top);
+            var scrolling = $(this).scrollTop(),
+              divoffset = $(".count").offset().top,
+              screenBottom = scrolling + $(window).height(), 
+              elemBottom = divoffset + $(".count").outerHeight (); // 
+            if (screenBottom > elemBottom) {
+              if (bAnimate) {
+                 $(".count").css ("opacity", "1.0");
+                countUp();
+                bAnimate = false;
+              }
             }
-        }, 40);
-    }
-
-    for (j = 0; j < countersQuantity; j++) {
-        count(0, counter[j], j);
-    }
-
+          })
+        })
 });
 
 //scroll top
