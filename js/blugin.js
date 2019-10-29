bfca3bdf34c872bd7827680f188f3da2c1cd693d
Amal-hossam <amlhossamy@gmail.com>
2019-10-29 01:36:55 -0700

    fix scroll bar

--- js/blugin.js
+++ js/blugin.js
@@ -3,10 +3,11 @@ $(document).ready(function () {
     //nice scroll
 
     $("Html").niceScroll();
-
+/*
     $(".carousel").carousel({
         interval: 2500
     });
+    */
     //navbar
 
     var scroll_start = 0;
